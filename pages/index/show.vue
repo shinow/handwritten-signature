ca37f8b42c14530c68e9855af3f5c23a7a86bbb7
zhaoyidong <zhaoyidongzdd@foxmail.com>
2020-04-29 19:55:12 +0800

    first commit

---
+++ pages/index/show.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="container">
+		<image :src="src">
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad: function(option) {
+			this.src = option.src
+			console.log(option.src)
+		},
+		data() {
+			return {
+				src: ''
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
